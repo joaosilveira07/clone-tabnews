ac0ef965e42d757c38c42cf507fa28c962f17dc7
João Pedro <joaopedrocva21@gmail.com>
2026-09-22 19:25:07 +0000

    subindo primeiras alterações

---
+++ pages/index.js
@@ -0,0 +1,15 @@
+function Home(){
+    return (
+        <div>
+            <h1>Teste</h1>
+            <HelloWorld></HelloWorld>
+        </div>
+    );
+}
+
+function HelloWorld(){
+    return <p>Hello World!</p>
+}
+
+export default Home;
+export { HelloWorld };
